37bddc37e03eae49d487a30a3e66c97c20c264ba
Bowbee <debenh.thoma@gmail.com>
2019-01-23 08:16:44 +1300

    yeety skeety header

--- bastionreact/src/components/PageHeader.js
+++ bastionreact/src/components/PageHeader.js
@@ -18,9 +18,34 @@ class PageHeader extends Component {
     render() {
         return (
             <div className="BossWrapper align-items-center">
-                <div className="PageHeader unselectable bg-dark rounded d-flex p-1 pt-2 mt-2 mx-2 align-items-center" onClick={this.onHeaderClick}>
-                    <img className="BossImage" alt="" />
-                    <h3 className="BossName text-light align-middle">Header</h3>
+                <div className="PageHeader unselectable bg-dark rounded d-flex mt-2 mx-2 align-items-center" onClick={this.onHeaderClick}>
+                    <div className="container px-0">
+                        <div className="row w-100 align-self-center d-flex justify-content-between mx-0">
+                            <div className="col-2 align-self-end px-1">
+                                <img className="yeetGif" src="src/dance.gif" height="32px" />
+                            </div>
+                            <div className="col-8 align-self-center">
+                                <div className="input-group m-3">
+                                    <div className="input-group-prepend">
+                                        <button class="btn btn-outline-secondary searchBar" type="button">
+                                            <i className="fas fa-arrow-down searchIcon"></i>
+                                        </button>
+                                    </div>
+                                    <input type="text" className="form-control searchBar text-light" placeholder="Yeet" aria-label="Search" />
+                                    <div className="input-group-append">
+                                        <button class="btn btn-outline-secondary searchBar" type="button">
+                                            <i className="fas fa-search searchIcon"></i>
+                                        </button>
+                                    </div>  
+                                </div>
+                            </div>
+                            <div className="col">
+                            </div>
+                            <div className="col-auto px-0 align-self-end px-1">
+                                <img className="yeetGif" src="src/ytho.gif" width="36px" />
+                            </div>                   
+                        </div>
+                    </div>
                 </div>
             </div>
         );
