10f6eb7c313b45118a8e510d59152fffb11397a2
Blightwidow <theo@dammaretz.fr>
2020-12-11 13:26:51 +0100

    Updates

---
+++ precache-manifest.03583012845f733756ef82061e29af3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e53f88188a67f138450f",
+    "url": "/static/css/main.59fe47c4.chunk.css"
+  },
+  {
+    "revision": "e53f88188a67f138450f",
+    "url": "/static/js/main.e53f8818.chunk.js"
+  },
+  {
+    "revision": "6ee273a32d8152403fe4",
+    "url": "/static/js/1.6ee273a3.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1a4d2917102cf2735e696514c30f6bf6",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.966b089760482d9efd75195824da4488.js"
+  "/precache-manifest.03583012845f733756ef82061e29af3b.js"
 );
 
 workbox.clientsClaim();
